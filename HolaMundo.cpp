dd0c41cd33af695d8f4e4e1a67dff6646fa7ad53
Wixo Akbal <wixoakbal@outlook.com>
2024-02-26 23:59:18 -0600

    Hola Mundo
    - Se agrega código en C++ de Hola Mundo

---
+++ HolaMundo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    cout << "Hola Mundo!!!" << endl;
+    system("pause");
+    return 0;
+}
